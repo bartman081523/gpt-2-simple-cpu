ae7f630aa0c2607cefe2ce923ad0c4bce45e5ec6
bartman081523 <43696399+bartman081523@users.noreply.github.com>
2022-09-10 13:09:47 +0200

    Update gpt_2.py

--- gpt_2_simple/gpt_2.py
+++ gpt_2_simple/gpt_2.py
@@ -121,8 +121,9 @@ def reset_session(sess, threads=-1, server=None):
 
 def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
-    return [x.name for x in local_device_protos if x.device_type == 'CPU']
-
+    #return [x.name for x in local_device_protos if x.device_type == 'GPU']
+    return ""
+    
 def finetune(sess,
              dataset,
              steps=-1,
